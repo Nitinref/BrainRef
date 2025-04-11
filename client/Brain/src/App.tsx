aa1b0c91b57283ff0805c79645eefe7df15d205f
Nitin Yadav <nitinyadav484220@gmail.com>
2025-04-11 21:47:28 +0530

    Update App.tsx

--- client/Brain/src/App.tsx
+++ client/Brain/src/App.tsx
@@ -1,20 +1,21 @@
 import { Dashboard } from "./pages/dashboard";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Frame from "./pages/Frame";
 
-function App(){
-
-return <BrowserRouter>
-<Routes>
-  <Route path = "/signup" element={<Signup />}/>
-  <Route path="/signin" element={<Signin />} />
-  <Route path="/dashboard" element={<Dashboard />} />
- <Route path="/Frame"  element={<Frame />} />
-
-</Routes>
-</BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/frame" element={<Frame />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
